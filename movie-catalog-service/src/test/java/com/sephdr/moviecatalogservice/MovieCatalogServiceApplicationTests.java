cb18bc8c0dd44da94130ba0fcf635d7ed70924d7
sephdr <jacdelosreyes013@gmail.com>
2022-04-11 19:22:49 +0800

    initial commit

---
+++ movie-catalog-service/src/test/java/com/sephdr/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sephdr.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
